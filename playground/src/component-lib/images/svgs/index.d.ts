d1a42094f7321be6b38bfb645b8f496ccad7b6a0
Rich <rclt201@gmail.com>
2020-06-27 18:57:49 +0100

    added playground

--- playground/src/component-lib/images/svgs/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Tick from './tick.svg';
-import Edit from './edit.svg';
-import Chevron from './chevron.svg';
-import Movie from './movie.svg';
-import Thumb from './thumb.svg';
-import Arrows from './arrows.svg';
-import Arrow from './arrow.svg';
-export { Tick, Edit, Chevron, Movie, Thumb, Arrows, Arrow };
